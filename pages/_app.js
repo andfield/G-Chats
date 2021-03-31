c3d3b70a134c1d3342d03db7aef0882a233c2401
andfield <siddharththakur76@gmail.com>
2021-03-31 16:04:06 +1300

    Check if user is logged in

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../firebase";
+import Login from './Login'
+import Loading from '../components/Loading'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //get the user if anyone is logged in
+  const [user, loading] = useAuthState(auth);
+
+  //if the user is being authenticated and its loading.
+  if(loading) return <Loading />;
+
+  //If the user doesnt exists
+  if (!user) return <Login />
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
