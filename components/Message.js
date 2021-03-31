2285e418e3f11ee020e2827dc41f7a5007656e32
andfield <siddharththakur76@gmail.com>
2021-04-01 02:22:06 +1300

    On Fire BABYYY

--- components/Message.js
+++ components/Message.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
+import {useAuthState} from "react-firebase-hooks/auth"
+import { auth } from '../firebase'
 
 function Message({user, message}) {
+    const [userLoggedIn] = useAuthState(auth);
     return (
         <Container>
             <p>{message.message}</p>
