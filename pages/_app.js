33616c579dc4d59e75be5d550c4372206ef528d9
andfield <siddharththakur76@gmail.com>
2021-03-31 16:20:22 +1300

    Save the user details when they first loggin

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,25 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase";
 import Login from './Login'
 import Loading from '../components/Loading'
+import { useEffect } from "react";
+import firebase from "firebase"
 
 function MyApp({ Component, pageProps }) {
   //get the user if anyone is logged in
   const [user, loading] = useAuthState(auth);
+
+    //when the user first signin capture there details using useEffect.
+    useEffect(() => {
+      if(user){
+        console.log(user)
+        db.collection("users").doc(user.uid).set({
+          name: user.displayName,
+          email: user.email,
+          lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+          photoURL: user.photoURL
+        }, {merge: true})
+      }
+    }, [user])
 
   //if the user is being authenticated and its loading.
   if(loading) return <Loading />;
