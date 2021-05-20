60ab772c672869d84b4cc420f9b4bef3b155a4f8
andfield <siddharththakur76@gmail.com>
2021-05-20 20:59:24 +1200

    send notification done

---
+++ hooks/userMessageNotification.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { db } from "../firebase";
+
+export default function UserMessageNotification(messageBody) {
+  const status = "active";
+
+
+  //Qury to find the user using props.
+
+}
